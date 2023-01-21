ad51dab707d162f150ca7ad6dd1bae4e34c5ec0b
davi_arttur <daviartur2004@gmail.com>
2023-01-21 13:47:02 -0300

    feat: new domain interfaces

--- src/domain/interfaces/index.ts
+++ src/domain/interfaces/index.ts
@@ -9,3 +9,22 @@ export interface Post {
   acessos: number,
   total_comments: string,
 }
+
+export interface HttpResponse<T> {
+  data?: T;
+  status?: number;
+  error?: string;
+}
+
+export interface BackendDefaultData {
+  message: string
+}
+
+export interface Http {
+  get:<T> () => Promise<HttpResponse<T>>
+  post:<T, Q> (body: Q) => Promise<HttpResponse<T>>
+  put:<T, Q> (body: Q) => Promise<HttpResponse<T>>
+  patch:<T, Q> (body: Q) => Promise<HttpResponse<T>>
+  delete:<T> () => Promise<HttpResponse<T>>
+  setToken: string
+}
